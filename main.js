91adb305336d32aa3eb9d03a8a43653a05cc0431
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 00:02:56 -0500

    Cleaned up the last technique. It wasn't correct before.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // var visitor = prompt("What you're name?");
 // var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. " + visitor + ", follow the white rabbit.";
 // document.write(message);
+// 
+//
+// var visitor = prompt("What you're name?");
+// var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. ";
+// message = message + visitor + ", follow the white rabbit.";
+// document.write(message);
+//
 
 
 var visitor = prompt("What you're name?");
 var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. ";
-message = message + visitor + ", follow the white rabbit.";
-document.write(message);
-
-
-
-var visitor = prompt("What you're name?");
-var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. ";
-message += visitor + ", follow the white rabbit.";
+message += visitor;
+message += ", follow the white rabbit.";
 document.write(message);
