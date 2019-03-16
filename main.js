83482f98583a83da0e5f1219e5dc0af5f63baa4a
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 01:05:10 -0500

    Created a web app in .js file that calculates how many seconds i've been a live for.

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-var shout = prompt("Tell me what you want me to yell back at you!");
-var shoutingBack = shout.toUpperCase();
-shoutingBack += "!";
-alert(shoutingBack);
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write("There are " + secondsPerDay + " seconds per day.");
+
+var yearsAlive = 28;
+var secondsAlive = secondsPerDay * daysPerWeek * weeksPerYear;
+secondsAlive *= yearsAlive;
+document.write("I've been a live for " + secondsAlive + " seconds!");
