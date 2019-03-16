9167bc42ead9a12c63f30ee0dc20c78288dd21d1
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 01:18:48 -0500

    Added a web app that created a sentence through asking questions and generating the sentence from the answers to the prompts.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var HTMLBadges = prompt('How many HTML Badges do you have?');
-var CSSBadges = prompt('How many CSS Badges do you have?');
-var totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
-alert("Wow you have " + totalBadges + "!");
+var adjective = prompt("Please type in an adjective");
+var noun = prompt("Please type in a nount");
+var verb = prompt("I need a verb, then im done.");
+alert("All done, Would yo like to see your sentence?");
+var sentence = "<h1> There once was a " + adjective;
+sentence += " programmer who wanted to use Javascript to " + verb;
+sentence += " the " + noun + "</h2>";
+document.write(sentence);
