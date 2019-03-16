11a626aeb2150aeffc946b8b1020dbb42b7b72c4
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 00:01:04 -0500

    Added two different techniques for concatenating strings.

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// var visitor = prompt("What you're name?");
+// var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. " + visitor + ", follow the white rabbit.";
+// document.write(message);
+
+
 var visitor = prompt("What you're name?");
-var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. " + visitor + ", follow the white rabbit.";
+var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. ";
+message = message + visitor + ", follow the white rabbit.";
+document.write(message);
+
+
+
+var visitor = prompt("What you're name?");
+var message = 'Hello ' + visitor + ". Did you know that combining strings is called, concatenation. ";
+message += visitor + ", follow the white rabbit.";
 document.write(message);
