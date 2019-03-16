50ad878224accd3c82fa14628acebef95a0208d7
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 01:24:42 -0500

    Created a question countdown tracker.

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-var adjective = prompt("Please type in an adjective");
-var noun = prompt("Please type in a nount");
-var verb = prompt("I need a verb, then im done.");
+var questions = 3;
+var questionsLeft = ' [' + questions + ' questions left.]';
+var adjective = prompt("Please type in an adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left.]';
+var noun = prompt("Please type in a noun" + questionsLeft);
+questions -= 1;
+questionsLeft = ' [' + questions + ' questions left.]';
+var verb = prompt("I need a verb, then im done." + questionsLeft);
 alert("All done, Would yo like to see your sentence?");
 var sentence = "<h1> There once was a " + adjective;
 sentence += " programmer who wanted to use Javascript to " + verb;
