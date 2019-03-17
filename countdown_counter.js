d0502fa80b1563e02f59a12f18194fd7c8f2184f
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 22:04:20 -0500

    Moved over some more code to countdown_counter.js file | Wrote out a quick if else conditional statement.

--- countdown_counter.js
+++ countdown_counter.js
@@ -12,3 +12,13 @@
 // sentence += " programmer who wanted to use Javascript to " + verb;
 // sentence += " the " + noun + "</h2>";
 // document.write(sentence);
+
+// var chosenNumber = prompt("type in a number that we can use for this shit.")
+// var randomNumber = Math.floor( Math.random() * parseInt(chosenNumber));
+// alert(randomNumber);
+// // var newNumber = Math.floor(Math.random() * randomNumber);
+// // alert(newNumber);
+
+// var chosenNumber = prompt("Type in a number");
+// var randomNumber = Math.floor(Math.random() * parseInt(chosenNumber));
+// console.log(randomNumber);
--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// var chosenNumber = prompt("type in a number that we can use for this shit.")
-// var randomNumber = Math.floor( Math.random() * parseInt(chosenNumber));
-// alert(randomNumber);
-// // var newNumber = Math.floor(Math.random() * randomNumber);
-// // alert(newNumber);
-
-var chosenNumber = prompt("Type in a number");
-var randomNumber = Math.floor(Math.random() * parseInt(chosenNumber));
-console.log(randomNumber);
+var answer = prompt("What coding language is the name of a gem?");
+if (answer == 3) {
+  document.write("<h1>That is correct! It's 3!</h1>");
+} else {
+  document.write("<h1>Sorry that's incorrect</h1>");
+}
