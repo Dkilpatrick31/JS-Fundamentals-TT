9c5f3cb6cbb0234c2feb51d33cb5f9fb6a7db720
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-17 00:54:37 -0500

    Stopping point for the night | Creating a quiz with tracking answers correctly | Giving the player a medal based on how many questions answered correctly. | Letting the player know which questions where answered correctly.

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-// var answer = prompt("What coding language is the name of a gem?");
-// if (answer == 3) {
-//   document.write("<h1>That is correct! It's 3!</h1>");
-// } else {
-//   document.write("<h1>Sorry that's incorrect</h1>");
-// }
+// ask 5 questions - DONE
+keep track of the # of questionsed answered correctly
+let the player know what questoins they got correctly.
+rank the players
+  gold - all correctly
+  silver - 3 to 4 correctly
+  bronze - 1 to 2 correctly
+  no crown - 0 correct
+
+var question1 = prompt("What color is the sky?");
+var answer1 = BLUE;
+var question2 = prompt("What is my Name?");
+var answer2 = Dane;
+var question3 = prompt('Are you alive?');
+var answer3 = yes;
+var question4 = prompt("Would you like to have a job already?");
+var answer4 = YES;
+var question5 = prompt("What language must you learn to do that?");
+var answer5 = Javascript;
--- randomNumberGuessingGame.js
+++ randomNumberGuessingGame.js
@@ -1,38 +1,38 @@
-// The Random Number Guess Game Generates a number between 1 and 6 and gives a player two attempts to guess the number.
-
-
-// assume the player did guess correctly
-var correctGuess = false;
-
-//  generate a random number from 1 to 6.
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-
-// get/prompt the player to guess a number between 1 and 6.
-var guess = prompt("Guess a random number between 1 and 6.");
-
-// if the number guessed is correct, then it's true.
-if (parseInt(guess) === randomNumber) {
-  correctGuess = true;
-
-// if the guessed number is less than the randomNumber. Prompt the player to guess higher.
-} else if (parseInt(guess) < randomNumber) {
-  var guessMore = prompt("Sorry but the number that I'm thinking of is more than " + guess + ". Guess again!");
-  if (parseInt(guessMore) === randomNumber) {
-    correctGuess = true;
-  }
-
-// if the guessed number is more than the randomNumber. Prompt the player to guess lower.
-} else if (parseInt(guess) > randomNumber) {
-  var guessLess = prompt("Sorry but the number I'm thinking of is less than " + guess + ". Guess again!");
-} if (parseInt(guessLess) === randomNumber) {
-  correctGuess = true;
-}
-
-// if the guessed number is the same as the randomNumber, then let the player know they won.
-if (correctGuess) {
-  document.write("<p>You guess the number!</p>");
-
-// if the player never guessed the right number. Let the player know what the randomNumber was.
-} else {
-  document.write("<p>Sorry the number was " + randomNumber + ".</p>" );
-}
+// // The Random Number Guess Game Generates a number between 1 and 6 and gives a player two attempts to guess the number.
+//
+//
+// // assume the player did guess correctly
+// var correctGuess = false;
+//
+// //  generate a random number from 1 to 6.
+// var randomNumber = Math.floor(Math.random() * 6) + 1;
+//
+// // get/prompt the player to guess a number between 1 and 6.
+// var guess = prompt("Guess a random number between 1 and 6.");
+//
+// // if the number guessed is correct, then it's true.
+// if (parseInt(guess) === randomNumber) {
+//   correctGuess = true;
+//
+// // if the guessed number is less than the randomNumber. Prompt the player to guess higher.
+// } else if (parseInt(guess) < randomNumber) {
+//   var guessMore = prompt("Sorry but the number that I'm thinking of is more than " + guess + ". Guess again!");
+//   if (parseInt(guessMore) === randomNumber) {
+//     correctGuess = true;
+//   }
+//
+// // if the guessed number is more than the randomNumber. Prompt the player to guess lower.
+// } else if (parseInt(guess) > randomNumber) {
+//   var guessLess = prompt("Sorry but the number I'm thinking of is less than " + guess + ". Guess again!");
+// } if (parseInt(guessLess) === randomNumber) {
+//   correctGuess = true;
+// }
+//
+// // if the guessed number is the same as the randomNumber, then let the player know they won.
+// if (correctGuess) {
+//   document.write("<p>You guess the number!</p>");
+//
+// // if the player never guessed the right number. Let the player know what the randomNumber was.
+// } else {
+//   document.write("<p>Sorry the number was " + randomNumber + ".</p>" );
+// }
