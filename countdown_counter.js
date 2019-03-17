8882bfddafcd36faf880ba8aeeb13f0995364a88
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-16 21:31:50 -0500

    Moved some code over to a new file countdown_counter.js for organizing purposes.

---
+++ countdown_counter.js
@@ -0,0 +1,14 @@
+// var questions = 3;
+// var questionsLeft = ' [' + questions + ' questions left.]';
+// var adjective = prompt("Please type in an adjective" + questionsLeft);
+// questions -= 1;
+// questionsLeft = ' [' + questions + ' questions left.]';
+// var noun = prompt("Please type in a noun" + questionsLeft);
+// questions -= 1;
+// questionsLeft = ' [' + questions + ' questions left.]';
+// var verb = prompt("I need a verb, then im done." + questionsLeft);
+// alert("All done, Would yo like to see your sentence?");
+// var sentence = "<h1> There once was a " + adjective;
+// sentence += " programmer who wanted to use Javascript to " + verb;
+// sentence += " the " + noun + "</h2>";
+// document.write(sentence);
--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-var questions = 3;
-var questionsLeft = ' [' + questions + ' questions left.]';
-var adjective = prompt("Please type in an adjective" + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left.]';
-var noun = prompt("Please type in a noun" + questionsLeft);
-questions -= 1;
-questionsLeft = ' [' + questions + ' questions left.]';
-var verb = prompt("I need a verb, then im done." + questionsLeft);
-alert("All done, Would yo like to see your sentence?");
-var sentence = "<h1> There once was a " + adjective;
-sentence += " programmer who wanted to use Javascript to " + verb;
-sentence += " the " + noun + "</h2>";
-document.write(sentence);
+// var chosenNumber = prompt("type in a number that we can use for this shit.")
+// var randomNumber = Math.floor( Math.random() * parseInt(chosenNumber));
+// alert(randomNumber);
+// // var newNumber = Math.floor(Math.random() * randomNumber);
+// // alert(newNumber);
+
+var chosenNumber = prompt("Type in a number");
+var randomNumber = Math.floor(Math.random() * parseInt(chosenNumber));
+console.log(randomNumber);
