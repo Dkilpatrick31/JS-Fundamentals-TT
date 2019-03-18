7d9090947b97a14d3a26e86c00159eaa4ef84852
Dane Kilpatrick <Dkilpatrick1990@gmail.com>
2019-03-18 16:37:40 -0500

    Created a simple function

--- functions.js
+++ functions.js
@@ -1,5 +1,19 @@
-function alertRandom() {
-  var randomNumber = Math.floor(Math.random() * 6) + 1;
-  alert(randomNumber);
+// function alertRandom() {
+//   var randomNumber = Math.floor(Math.random() * 6) + 1;
+//   alert(randomNumber);
+// }
+// alertRandom();
+
+function isEmailEmpty() {
+  var field = document.getElementById('email');
+  if (field.value === '') {
+    return true;
+  } else {
+    return false;
+  }
 }
-alertRandom();
+
+var fieldTest = isEmailEmpty();
+if (field === true) {
+  alert('please provide your email address');
+}
